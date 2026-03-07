1dd38d47be877bda06912716aea8e21cc0598535
liebertwalter <mehmettugturk@gmail.com>
2026-03-07 18:10:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-const CACHE_NAME = "muhasebecin-v500";
-
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
@@ -8,7 +6,11 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.map((key) => caches.delete(key)))
-    ).then(() => self.clients.claim())
+    ).then(async () => {
+      const clients = await self.clients.matchAll({ type: "window" });
+      clients.forEach((client) => client.navigate(client.url));
+      return self.clients.claim();
+    })
   );
 });
 
