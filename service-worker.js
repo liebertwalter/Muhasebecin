0e3a713c1adbd8c547df3602f186bdbb076563f7
liebertwalter <mehmettugturk@gmail.com>
2026-03-07 17:23:08 +0300

    Change cache name and modify fetch event handling
    Updated cache name and removed file caching.

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,15 @@
-const CACHE_NAME = "muhasebecin-cache-v99";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./database.js",
-  "./voice.js",
-  "./notifications.js",
-  "./charts.js",
-  "./export.js",
-  "./backup.js",
-  "./auth.js",
-  "./utils.js",
-  "./manifest.json",
-  "./firebase.js"
-];
+const CACHE_NAME = "muhasebecin-v500";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map((key) => caches.delete(key)))
     ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener("fetch", () => {});
